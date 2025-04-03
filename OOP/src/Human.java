87cb6c5ec5cd5767db21cccb0d73dd293e44e457
FursovArtem <131772117+FursovArtem@users.noreply.github.com>
2025-04-03 17:21:19 +0400

    OOP day 1

---
+++ OOP/src/Human.java
@@ -0,0 +1,43 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Human {
+    String lastName;
+    String firstName;
+    int age;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Human(String lastName, String firstName, int age) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.age = age;
+        System.out.println("HConstructor: " + Integer.toHexString(this.hashCode()));
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ": " + lastName + " " + firstName + " " + age;
+    }
+}
