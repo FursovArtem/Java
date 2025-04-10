79feaf9a76a897bc193d2b3c95bd04cc3d5c7d2c
FursovArtem <131772117+FursovArtem@users.noreply.github.com>
2025-04-10 16:44:26 +0400

    awt

--- DesktopAWT/src/main/java/org/example/MainWindow.java
+++ DesktopAWT/src/main/java/org/example/MainWindow.java
@@ -1,25 +1,22 @@
 package org.example;
 
+import javax.swing.*;
 import java.awt.*;
 
-public class MainWindow extends Frame {
+public class MainWindow extends JFrame {
     public MainWindow() throws HeadlessException {
         this.setBounds(200, 200, 400, 300);
-        this.setLayout(null);
+        this.setLayout((LayoutManager) null);
         this.setVisible(true);
         this.setTitle("AWT - Abstract Window Toolkit");
-
         Label lblLastName = new Label("Last Name: ");
         Label lblFirstName = new Label("First Name: ");
         Label lblBirthDate = new Label("Birth Date: ");
-
         TextField textLastName = new TextField();
         TextField textFirstName = new TextField();
         TextField textBirthDate = new TextField();
-
         Button btnSubmit = new Button("Submit");
         Button btnReset = new Button("Reset");
-
         lblLastName.setBounds(20, 50, 100, 24);
         lblFirstName.setBounds(20, 100, 100, 24);
         lblBirthDate.setBounds(20, 150, 100, 24);
@@ -28,7 +25,6 @@ public class MainWindow extends Frame {
         textBirthDate.setBounds(120, 150, 100, 24);
         btnSubmit.setBounds(20, 200, 100, 32);
         btnReset.setBounds(120, 200, 100, 32);
-
         this.add(lblLastName);
         this.add(lblFirstName);
         this.add(lblBirthDate);
